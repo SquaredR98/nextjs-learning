fb7ca04f4a9a777f2419d6c250b59d3ff536dacb
SquaredR98 <mail@ravi-ranjan.in>
2023-12-28 18:32:08 +0530

    Recommit

--- discuss/src/app/topics/[slug]/posts/[postId]/page.tsx
+++ discuss/src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { paths } from "@/paths";
 import ShowPost from "@/components/posts/ShowPost";
 import CommentList from "@/components/comments/ListComment";
-import { fetchCommentsByPostId } from "@/db/queries/comments";
 import CreateCommentForm from "@/components/comments/CreateCommentForm";
 
 interface IShowPostPageProps {
@@ -20,7 +19,7 @@ export default function PostShow({ params }: IShowPostPageProps) {
       <Link href={paths.showTopic(slug)}>Back to topic: {slug}</Link>
       <ShowPost postId={postId} />
       <CreateCommentForm postId={postId} />
-      <CommentList fetchData={() => fetchCommentsByPostId(postId)} />
+      <CommentList postId={postId} />
     </div>
   );
 }
